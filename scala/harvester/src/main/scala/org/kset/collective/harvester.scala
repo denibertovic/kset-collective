47f4025a92d3bc3476001ca952131befe29545c6
tmarice <tmarice@gmail.com>
2012-06-13 00:01:37 +0200

    dodana tail funkcionalnost

--- scala/harvester/src/main/scala/org/kset/collective/harvester.scala
+++ scala/harvester/src/main/scala/org/kset/collective/harvester.scala
@@ -1,33 +1,33 @@
 package org.kset.collective.harvester
 
 import net.contentobjects.jnotify.{JNotifyListener, JNotify}
+import java.io._
 import akka.actor._
 import akka.routing._
+
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
+import scala.math._
+
 
 object Harvester extends App {
   sealed trait HarvesterMessage
   case object StartMaster extends HarvesterMessage
   case object StopMaster extends HarvesterMessage
-  case class StartWatch(watchee: String, num: Int) extends HarvesterMessage
+  case class StartWatch(watcheeFolder: String, watcheeFileList: HashSet[String]) extends HarvesterMessage
   case class StopWatch(watchID: Int) extends HarvesterMessage
 
   start
 
   class MasterActor extends Actor {
-    val files:  Array[String] = Array("/home/tm/new_file1", "/home/tm/new_file2")
-    def start = {
-//      val router = context.actorOf(Props[Watcher].withRouter(RoundRobinRouter(files.size)), "watcher")
-        val act1 = context.actorOf(Props[Watcher], name = "act1")
-        val act2 = context.actorOf(Props[Watcher], name = "act2")
+    val folders: Array[String] = Array("/home/tmarice/programming/kset/kset-collective/test")
+    val watchList = new HashMap[String, HashSet[String]]
+    watchList += "/home/tmarice/programming/kset/kset-collective/test/" -> HashSet("file1.log", "file2.log")
 
-      var a = 1
-      for (file <- files) {
-        if (a == 1) 
-          act1 ! StartWatch(file, a)
-        else 
-          act2 ! StartWatch(file, a)
-//        router ! StartWatch(file, a)
-        a += 1
+    def start = {
+      val router = context.actorOf(Props[Watcher].withRouter(RoundRobinRouter(folders.size)), "watcher")
+      for (watchPair <- watchList) {
+        router ! StartWatch(watchPair._1, watchPair._2); 
       }
     }
 
@@ -43,12 +43,11 @@ object Harvester extends App {
   class Watcher extends Actor {
 
     def receive = {
-      case StartWatch(watchee, num) => start(watchee, num)
+      case StartWatch(watcheeFolder, watcheeFileList) => start(watcheeFolder, watcheeFileList)
       case StopWatch(watchID) => stop(watchID)
     }
 
-    def start(watchee: String, num: Int) = {
-      println(watchee)
+    def start(watcheeFolder: String, watcheeFileList: HashSet[String]) = {
       val mask = JNotify.FILE_CREATED  |
       JNotify.FILE_DELETED  |
       JNotify.FILE_MODIFIED |
@@ -56,10 +55,8 @@ object Harvester extends App {
 
       val watchSubtree = true;
 
-      val watchID = JNotify.addWatch(watchee, mask, watchSubtree, new Listener(num));
+      val watchID = JNotify.addWatch(watcheeFolder, mask, watchSubtree, new Listener(watcheeFolder, watcheeFileList));
 
-//      Thread.sleep(1000000);
-    self ! StopWatch(watchID)
     }
 
     def stop(watchID: Int ) = {
@@ -78,21 +75,42 @@ object Harvester extends App {
 
 }
 
-class Listener(num: Int) extends JNotifyListener {
-  println(num)
+class Listener(watcheeFolder: String, watcheeFileList: HashSet[String]) extends JNotifyListener {
   def fileRenamed(wd: Int, rootPath: String, oldName: String, newName: String) {
-    println("renamed " + rootPath + " : " + oldName + " -> " + newName);
+    if (watcheeFileList.contains(oldName))
+      println("renamed " + rootPath + " : " + oldName + " -> " + newName);
+      
   }
 
   def fileModified(wd: Int, rootPath: String, name: String) {
-    println("modified " + rootPath + " : " + name + " NUM " + num)
+    if (watcheeFileList.contains(name))
+      println("modified " + rootPath + " : " + name)
+
+      val file = new File(rootPath + "/" + name)
+      val rfile = new RandomAccessFile(rootPath + "/" + name, "r")
+      val pos = max(file.length() - 1024, 0)
+      rfile.seek(pos)
+
+      var line :String = "\n"
+      var res  :String = ""
+      line = rfile.readLine() 
+      while (line != null) {
+        res += line + "\n"
+        line = rfile.readLine()
+      }
+
+      val lines = res.split('\n').toList
+      
+      println(lines.last)
   }
 
   def fileDeleted(wd: Int, rootPath: String, name: String) {
-    println("deleted " + rootPath + " : " + name)
+    if (watcheeFileList.contains(name))
+      println("deleted " + rootPath + " : " + name)
   }
 
   def fileCreated(wd: Int, rootPath: String, name: String) {
-    println("created " + rootPath + " : " + name)
+    if (watcheeFileList.contains(name))
+      println("created " + rootPath + " : " + name)
   }
 }
